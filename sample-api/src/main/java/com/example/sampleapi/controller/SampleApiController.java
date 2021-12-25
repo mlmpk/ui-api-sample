7c0633c752eed90508550468bbf193780f59380d
Maik Lempik <mlempik@gmail.com>
2021-12-25 14:44:32 +0100

    Add PostMapping method

--- sample-api/src/main/java/com/example/sampleapi/controller/SampleApiController.java
+++ sample-api/src/main/java/com/example/sampleapi/controller/SampleApiController.java
@@ -1,20 +1,29 @@
 package com.example.sampleapi.controller;
 
+import com.example.sampleapi.model.Formdata;
 import com.example.sampleapi.model.SampleData;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@CrossOrigin("http://localhost:3000")
 public class SampleApiController {
 
     @GetMapping("/getData")
     public ResponseEntity<SampleData> getSampleData(@RequestParam(value = "param",required = false) String parameter){
         if(parameter != null && !parameter.isEmpty()) {
-            return ResponseEntity.ok(new SampleData(1,"this is a sample with parameter.",parameter));
+            return ResponseEntity.ok(new SampleData(1,"this is a sample with parameter.", parameter));
         } else {
-            return ResponseEntity.ok(new SampleData(1, "This is a just a sample.!"));
+            return ResponseEntity.ok(new SampleData(1, "This is a simple sample!"));
+        }
+    }
+
+    @PostMapping("/create")
+    public ResponseEntity<SampleData> postData(@RequestBody Formdata body){
+        if(body != null && !body.getVal().isEmpty()) {
+            return ResponseEntity.ok(new SampleData(1,"this is a sample with body.", body.getVal()));
+        } else {
+            return ResponseEntity.badRequest().build();
         }
     }
 
