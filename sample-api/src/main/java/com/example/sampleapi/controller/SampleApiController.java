8b66133055339ff69f5da79a07456ebf1f77f1b1
Maik Lempik <mlempik@gmail.com>
2021-12-25 11:08:43 +0100

    Remove unused import

--- sample-api/src/main/java/com/example/sampleapi/controller/SampleApiController.java
+++ sample-api/src/main/java/com/example/sampleapi/controller/SampleApiController.java
@@ -3,7 +3,6 @@ package com.example.sampleapi.controller;
 import com.example.sampleapi.model.SampleData;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
